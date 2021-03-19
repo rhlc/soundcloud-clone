6e8683fded8fbda6c8909980a9dd636c83daeb26
Rahul Choudhary <rhlc@outlook.in>
2021-03-19 16:06:01 +0530

    day 1 progress

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import configureStore from "./stores/configureStore";
+import * as actions from "./actions";
+import Stream from "./components/Stream";
+
+const tracks = [
+  {
+    title: "Some track",
+  },
+  {
+    title: "Some other track",
+  },
+];
+
+const store = configureStore();
+store.dispatch(actions.setTracks(tracks));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Stream />
+  </Provider>,
+  document.getElementById("root")
 );
 
 module.hot.accept();
